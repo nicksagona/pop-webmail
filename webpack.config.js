60d63c649b8c82f6cf548ff4f9f2ed1fa6524670
Ralph Schindler <ralph@ralphschindler.com>
2018-01-25 12:13:04 -0600

    updated to work with master

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: ['./app/ui/app.js', './app/ui/app.scss'],
+  entry: ['./app/resources/app.js', './app/resources/app.scss'],
   output: {
     path: path.resolve(__dirname, './public/assets'),
     filename: 'app.js'
